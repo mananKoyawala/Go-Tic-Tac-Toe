9ab4aef1297107cba1d9c56ae9be07df3d6381b0
manan5565 <manankoyawala5565@gmail.com>
2024-03-03 17:55:32 +0530

    Tic-Tac-Toe Game Completed

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ var isPlayer1 = true
 
 func main() {
 	player_array := [3][3]string{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}}
-	// player_array := [3][3]interface{}{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
 
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -44,6 +43,57 @@ func main() {
 
 func isGameOver(player_array *[3][3]string) {
 	// hasNoInteger := false
+
+	// Check row
+	for i := 0; i < 3; i++ {
+		if player_array[i][0] == player_array[i][1] && player_array[i][1] == player_array[i][2] {
+			if player_array[i][1] == "O" {
+				color.Green("Player 1 is Win!")
+				PrintCanvas(player_array)
+				os.Exit(0)
+			}
+			if player_array[i][1] == "X" {
+				color.Green("Player 2 is Win!")
+				PrintCanvas(player_array)
+				os.Exit(0)
+			}
+		}
+	}
+
+	// Check column
+	for j := 0; j < 3; j++ {
+		if player_array[0][j] == player_array[1][j] && player_array[1][j] == player_array[2][j] {
+			if player_array[1][j] == "O" {
+				color.Green("Player 1 is Win!")
+				PrintCanvas(player_array)
+				os.Exit(0)
+			}
+			if player_array[1][j] == "X" {
+				color.Green("Player 2 is Win!")
+				PrintCanvas(player_array)
+				os.Exit(0)
+			}
+		}
+	}
+
+	// Check diagonal
+	if (player_array[0][0] == player_array[1][1] && player_array[1][1] == player_array[2][2]) ||
+		(player_array[0][2] == player_array[1][1] && player_array[1][1] == player_array[2][0]) {
+		if player_array[1][1] != " " {
+			if player_array[1][1] == "O" {
+				color.Green("Player 1 is Win!")
+				PrintCanvas(player_array)
+				os.Exit(0)
+			}
+			if player_array[1][1] == "X" {
+				color.Green("Player 2 is Win!")
+				PrintCanvas(player_array)
+				os.Exit(0)
+			}
+
+		}
+	}
+
 	count := 0
 	for _, row := range player_array {
 		for _, val := range row {
